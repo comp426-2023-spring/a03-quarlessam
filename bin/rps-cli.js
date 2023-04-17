ed85c89671042ba86b8e3583c7d64b0dff2b1e0f
quarlessam <quarlessam@gmail.com>
2023-04-16 23:10:53 -0400

    rules fix

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,4 +5,8 @@ import { rpsls } from "../lib/rpsls.js"
 import minimist from 'minimist'
 
 const args = minimist(process.argv.slice(2))
+if (args.r || args.rules) {
+  rps(rules)
+  return
+}
 console.log(rps(args._[0]))
