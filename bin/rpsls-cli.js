ebb6595794ae38abd564690bc7594022c1e706ca
quarlessam <quarlessam@gmail.com>
2023-04-16 23:15:14 -0400

    i forgot another exit

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -7,6 +7,6 @@ import minimist from 'minimist'
 const args = minimist(process.argv.slice(2))
 if (args.r || args.rules) {
   rpsls('rules')
-  return
+  process.exit()
 }
 console.log(rpsls(args._[0]))
